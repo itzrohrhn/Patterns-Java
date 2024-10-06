d8c1cb0db732de51f0daf691124271769969afc7
Rohh the Developer / <106047683+itzrohrhn@users.noreply.github.com>
2024-10-06 12:24:53 +0530

    Add files via upload

---
+++ InvertedRightHalfPyramid.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class InvertedRightHalfPyramid {
+    public static void main(String[] args) {
+        System.out.print("Enter the number of Rows: ");
+        Scanner inp = new Scanner(System.in);
+        int rowSize = inp.nextInt();
+        inp.close();
+        for(int row=1; row<=rowSize; row++){
+            for(int col=row; col<=rowSize; col++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
